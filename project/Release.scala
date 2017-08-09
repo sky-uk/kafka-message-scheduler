c987fc64b388a0ba5363070fb4fe668cb3c41932
Oliver Lockwood <oliver.lockwood@sky.uk>
2017-08-09 09:23:59 +0100

    Put plugins together

--- project/Release.scala
+++ project/Release.scala
@@ -1,10 +1,38 @@
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport.{Docker => docker}
 import com.typesafe.sbt.packager.universal.UniversalPlugin.autoImport.Universal
+import com.typesafe.sbt.GitPlugin.autoImport.{git, showCurrentGitBranch}
 import sbt.Keys._
 import sbt.{Project, State, ThisBuild, taskKey}
 import sbtrelease.ReleasePlugin.autoImport._
 import sbtrelease.ReleaseStateTransformations.{runTest, setReleaseVersion => _, _}
 import sbtrelease._
+
+object Git {
+  // Prefix our release tags with `kafka-message-scheduler`
+  val VersionRegex = "kafka-message-scheduler-([0-9]+.[0-9]+.[0-9]+)-?(.*)?".r
+
+  // Maven convention says that if you're working on `a.b.c-SNAPSHOT`, the NEXT RELEASE will be `a.b.c`.
+  // The default behaviour of sbt-git says if the most recent release was `a.b.c`, you're working on `a.b.c-SNAPSHOT`.
+  //
+  // Since sbt-git is breaking convention, we work around by bumping the inferred version using our chosen version
+  // bumping strategy (sbt-release's implementation saves us from having to write too much code).
+  def bumpVersion(inputVersion: String): String = {
+    Version.apply(inputVersion)
+      .map(_.bump(Version.Bump.Minor).string)
+      .getOrElse(versionFormatError)
+  }
+
+  lazy val gitSettings = Seq(
+    showCurrentGitBranch,
+    git.baseVersion := "0.0.0",
+    git.useGitDescribe := true,
+    git.gitTagToVersionNumber := {
+      case VersionRegex(v, "") => Some(v)
+      case VersionRegex(v, _) => Some(s"${bumpVersion(v)}-SNAPSHOT")
+      case _ => None
+    }
+  )
+}
 
 object Release {
   // Useful tasks to show what versions would be used if a release was performed.
@@ -24,9 +52,7 @@ object Release {
       setReleaseVersion,
       runTest,
       tagRelease,
-// Insert release release phases (JARs or Docker images as appropriate)
-//      publishArtifacts,
-//      ReleaseStep(releaseStepTask(publish in docker)),
+      ReleaseStep(releaseStepTask(publish in docker)),
       pushChanges
     ),
     showReleaseVersion := { val rV = releaseVersion.value.apply(version.value); println(rV); rV },
--- project/javaagent.sbt
+++
@@ -1,1 +0,0 @@
-addSbtPlugin("com.lightbend.sbt" % "sbt-javaagent" % "0.1.3")

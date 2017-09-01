ede2d733093aa677c050ed436f30b88452697fc0
Lawrence Carvalho <Lawrence.Carvalho@sky.uk>
2017-09-01 10:59:39 +0100

    Revert code changes to kafka source as this logic can be done purely from configuration.

--- scheduler/src/main/scala/com/sky/kms/kafka/KafkaStream.scala
+++ scheduler/src/main/scala/com/sky/kms/kafka/KafkaStream.scala
@@ -8,38 +8,19 @@ import akka.kafka.{ConsumerSettings, ProducerSettings, Subscriptions}
 import akka.stream.scaladsl.{Sink, Source}
 import com.sky.kms.config.SchedulerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.{ByteArrayDeserializer, ByteArraySerializer, StringDeserializer}
 
-import scala.collection.JavaConverters._
 import scala.concurrent.Future
 
 object KafkaStream {
 
-  type Offset = Long
-
-  def beginningOffsetSource[T](config: SchedulerConfig)(implicit system: ActorSystem, crDecoder: ConsumerRecordDecoder[T]): Source[T, Control] = {
-    val consumerSettings = ConsumerSettings(system, new StringDeserializer, new ByteArrayDeserializer)
+  def source[T](config: SchedulerConfig)(implicit system: ActorSystem, crDecoder: ConsumerRecordDecoder[T]): Source[T, Control] =
     Consumer.plainSource(
-      consumerSettings,
-      Subscriptions.assignmentWithOffset(beginningPartitionPositions(config.scheduleTopic, consumerSettings))
+      ConsumerSettings(system, new StringDeserializer, new ByteArrayDeserializer),
+      Subscriptions.topics(config.scheduleTopic)
     ).map(crDecoder(_))
-  }
-
-  private def beginningPartitionPositions(topic: String, settings: ConsumerSettings[String, Array[Byte]]): Map[TopicPartition, Offset] = {
-    val consumer = settings.createKafkaConsumer()
-
-    consumer.subscribe(List(topic).asJava)
-    consumer.poll(1)
-
-    val partitions = consumer.assignment
-    consumer.seekToBeginning(partitions)
-    val partitionPositions = partitions.asScala.map(p => p -> consumer.position(p)).toMap
-
-    consumer.close()
-    partitionPositions
-  }
 
   def sink(implicit system: ActorSystem): Sink[ProducerRecord[Array[Byte], Array[Byte]], Future[Done]] =
     Producer.plainSink(ProducerSettings(system, new ByteArraySerializer, new ByteArraySerializer))
+
 }

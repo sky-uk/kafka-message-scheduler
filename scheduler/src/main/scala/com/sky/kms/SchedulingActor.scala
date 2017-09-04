9803f93c5a9fb5911b5b59befbbd3edc214e24db
Lawrence Carvalho <Lawrence.Carvalho@sky.uk>
2017-09-04 16:12:49 +0100

    address review comments.

--- scheduler/src/main/scala/com/sky/kms/SchedulingActor.scala
+++ scheduler/src/main/scala/com/sky/kms/SchedulingActor.scala
@@ -5,7 +5,6 @@ import java.time.temporal.ChronoUnit
 import java.util.concurrent.TimeUnit
 
 import akka.actor._
-import akka.pattern.pipe
 import akka.stream.QueueOfferResult._
 import akka.stream.scaladsl.SourceQueue
 import cats.syntax.show._
@@ -15,6 +14,7 @@ import com.sky.kms.domain._
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration.FiniteDuration
+import scala.util.Success
 
 class SchedulingActor(queue: SourceQueue[(String, ScheduledMessage)], akkaScheduler: Scheduler) extends Actor with ActorLogging {
 
@@ -47,7 +47,7 @@ class SchedulingActor(queue: SourceQueue[(String, ScheduledMessage)], akkaSchedu
         schedules - scheduleId
     }
 
-    (handleSchedulingMessage andThen updateStateAndAck) orElse handleInternalMessages
+    (handleSchedulingMessage andThen updateStateAndAck) orElse handleTrigger
   }
 
   private def updateStateAndAck(schedules: Map[ScheduleId, Cancellable]): Unit = {
@@ -55,17 +55,14 @@ class SchedulingActor(queue: SourceQueue[(String, ScheduledMessage)], akkaSchedu
     sender ! Ack
   }
 
-  private val handleInternalMessages: Receive = {
+  private val handleTrigger: Receive = {
     case Trigger(scheduleId, schedule) =>
       log.info(s"$scheduleId is due. Adding schedule to queue. Scheduled time was ${schedule.time}")
-      val queueOfferFuture = queue.offer((scheduleId, messageFrom(schedule)))
-        .map(ScheduleQueueOfferResult(scheduleId, _))
-        .recover { case t => ScheduleOfferException(scheduleId, t) }
-      queueOfferFuture pipeTo self
-    case res: ScheduleQueueOfferResult if res.queueOfferResult != Enqueued =>
-      log.warning(res.show)
-    case ScheduleOfferException(scheduleId, t) =>
-      log.warning(s"Failed to enqueue $scheduleId. ${t.getMessage}")
+      queue.offer((scheduleId, messageFrom(schedule))) onComplete {
+        case Success(Enqueued) => log.info(ScheduleQueueOfferResult(scheduleId, Enqueued).show)
+        case Success(res) => log.warning(ScheduleQueueOfferResult(scheduleId, res).show)
+        case scala.util.Failure(t) => log.warning(s"Failed to enqueue $scheduleId. ${t.getMessage}")
+      }
   }
 
   private def cancel(scheduleId: ScheduleId, schedules: Map[ScheduleId, Cancellable]): Boolean =
@@ -89,8 +86,6 @@ object SchedulingActor {
   case class Cancel(scheduleId: ScheduleId) extends SchedulingMessage
 
   private case class Trigger(scheduleId: ScheduleId, schedule: Schedule)
-
-  private case class ScheduleOfferException(scheduleId: ScheduleId, t: Throwable) extends Exception(t)
 
   case object Init
 

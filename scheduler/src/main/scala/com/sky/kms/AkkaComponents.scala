fd10ee959bbbbf154dbfae204929b9d15af2c6b6
Lawrence Carvalho <lacarvalho91@gmail.com>
2018-01-18 13:10:32 +0000

    add logging to stream supervisor

--- scheduler/src/main/scala/com/sky/kms/AkkaComponents.scala
+++ scheduler/src/main/scala/com/sky/kms/AkkaComponents.scala
@@ -1,12 +1,18 @@
 package com.sky.kms
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.Supervision.Stop
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import com.typesafe.scalalogging.LazyLogging
 
-trait AkkaComponents {
+trait AkkaComponents extends LazyLogging {
 
   implicit lazy val system = ActorSystem("kafka-message-scheduler")
 
-  implicit lazy val materializer = ActorMaterializer()
+  implicit lazy val materializer = ActorMaterializer(
+    ActorMaterializerSettings(system).withSupervisionStrategy(t => {
+      logger.error("Exception caught by stream supervisor", t)
+      Stop
+    }))
 
 }

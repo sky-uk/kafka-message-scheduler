e8f39bf4c66be29597b492e81462b8265c90d0dc
Lawrence Carvalho <Lawrence.Carvalho@sky.uk>
2018-02-21 19:41:21 +0000

    make kamon reporter config driven

--- scheduler/src/main/scala/com/sky/kms/SchedulerApp.scala
+++ scheduler/src/main/scala/com/sky/kms/SchedulerApp.scala
@@ -6,7 +6,6 @@ import com.sky.kms.actors.{PublisherActor, SchedulingActor, TerminatorActor}
 import com.sky.kms.config.Configured
 import com.sky.kms.streams.{ScheduleReader, ScheduledMessagePublisher}
 import kamon.Kamon
-import kamon.prometheus.PrometheusReporter
 
 case class SchedulerApp(reader: ScheduleReader, publisher: ScheduledMessagePublisher, publisherActor: ActorRef)
 
@@ -26,7 +25,7 @@ object SchedulerApp {
   }
 
   def run(implicit system: ActorSystem, mat: ActorMaterializer): Start[Running] = {
-    Kamon.addReporter(new PrometheusReporter)
+    Kamon.loadReportersFromConfig()
     ShutdownTasks.forKamon
 
     for {

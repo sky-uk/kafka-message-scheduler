1b16ce694af665b7e2623f4c9c6f3f3e29a3ab0c
Manuel cueto Rodriguez <manuel.cuetorodriguez@sky.uk>
2020-01-10 11:00:30 +0000

    handle queue backpressure on publisherActor

--- scheduler/src/main/scala/com/sky/kms/actors/PublisherActor.scala
+++ scheduler/src/main/scala/com/sky/kms/actors/PublisherActor.scala
@@ -2,20 +2,23 @@ package com.sky.kms.actors
 
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 import akka.stream.QueueOfferResult
+import akka.pattern.pipe
 import akka.stream.scaladsl.SourceQueueWithComplete
 import cats.syntax.show._
 import com.sky.kms.Start
-import com.sky.kms.actors.PublisherActor.{DownstreamFailure, Init, ScheduleQueue, Trigger}
+import com.sky.kms.actors.PublisherActor.{BufferUnblocked, DownstreamFailure, Init, ScheduleQueue, Trigger}
 import com.sky.kms.domain.PublishableMessage.ScheduledMessage
 import com.sky.kms.domain.{ScheduleEvent, ScheduleId, ScheduleQueueOfferResult}
 
 import scala.util.{Failure, Success}
+import scala.concurrent.Future
+import akka.actor.Stash
 
-class PublisherActor extends Actor with ActorLogging {
+class PublisherActor extends Actor with ActorLogging with Stash {
 
-  implicit val ec = context.dispatcher
-
-  override def receive: Receive = waitForQueue
+  implicit val ec                         = context.dispatcher
+  var stttt: Future[BufferUnblocked.type] = null
+  override def receive: Receive           = waitForQueue
 
   private def waitForQueue: Receive = {
     case Init(queue) =>
@@ -23,17 +26,30 @@ class PublisherActor extends Actor with ActorLogging {
       context become (receiveWithQueue(queue) orElse stopOnError)
   }
 
+  private def waitingState(queue: ScheduleQueue): Receive = stopOnError orElse {
+    case BufferUnblocked =>
+      context become (receiveWithQueue(queue) orElse stopOnError)
+      unstashAll()
+    case _ => stash()
+  }
+
   private def receiveWithQueue(queue: ScheduleQueue): Receive = {
     case Trigger(scheduleId, schedule) =>
-      queue.offer((scheduleId, messageFrom(schedule))) onComplete {
-        case Success(QueueOfferResult.Enqueued) =>
-          log.debug(ScheduleQueueOfferResult(scheduleId, QueueOfferResult.Enqueued).show)
-        case Success(res) =>
-          log.warning(ScheduleQueueOfferResult(scheduleId, res).show)
-        case Failure(t) =>
-          log.error(t, s"Failed to enqueue $scheduleId")
-          self ! DownstreamFailure(t)
-      }
+      context become waitingState(queue)
+      queue
+        .offer((scheduleId, messageFrom(schedule)))
+        .transformWith {
+          case Success(QueueOfferResult.Enqueued) =>
+            log.debug(ScheduleQueueOfferResult(scheduleId, QueueOfferResult.Enqueued).show)
+            Future.successful(BufferUnblocked)
+          case Success(res) =>
+            log.warning(ScheduleQueueOfferResult(scheduleId, res).show)
+            Future.successful(BufferUnblocked)
+          case Failure(t) =>
+            log.error(t, s"Failed to enqueue $scheduleId")
+            self ! DownstreamFailure(t)
+            Future.successful(BufferUnblocked)
+        } pipeTo self
   }
 
   private def stopOnError: Receive = {
@@ -53,6 +69,8 @@ object PublisherActor {
   case class Init(queue: ScheduleQueue)
 
   case class Trigger(scheduleId: ScheduleId, schedule: ScheduleEvent)
+
+  case object BufferUnblocked
 
   case class DownstreamFailure(t: Throwable)
 

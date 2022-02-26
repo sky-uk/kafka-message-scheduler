09a308b6aa7002a42a9783b248c0ee5275a5e99c
Ben Carter <benjohn.carter@gmail.com>
2022-02-26 20:01:21 +0000

    update specbase

--- scheduler/src/test/scala/com/sky/kms/base/SpecBase.scala
+++ scheduler/src/test/scala/com/sky/kms/base/SpecBase.scala
@@ -2,6 +2,8 @@ package com.sky.kms.base
 
 import com.danielasfregola.randomdatagenerator.RandomDataGenerator
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.BeforeAndAfterAll
 
-trait SpecBase extends WordSpecLike with Matchers with BeforeAndAfterAll with RandomDataGenerator with ScalaFutures
+trait SpecBase extends AnyWordSpec with Matchers with BeforeAndAfterAll with RandomDataGenerator with ScalaFutures

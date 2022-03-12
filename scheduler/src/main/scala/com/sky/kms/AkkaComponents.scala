6d4f0f83ec5d885d71a64a94aef8e6738fa9a6cd
Ben Carter <benjohn.carter@gmail.com>
2022-03-12 17:41:17 +0000

    Remove unused EC

--- scheduler/src/main/scala/com/sky/kms/AkkaComponents.scala
+++ scheduler/src/main/scala/com/sky/kms/AkkaComponents.scala
@@ -3,11 +3,8 @@ package com.sky.kms
 import akka.actor.ActorSystem
 import com.typesafe.scalalogging.LazyLogging
 
-import scala.concurrent.ExecutionContext
-
 trait AkkaComponents extends LazyLogging {
 
-  implicit lazy val system: ActorSystem  = ActorSystem("kafka-message-scheduler")
-  implicit lazy val ec: ExecutionContext = system.dispatcher
+  implicit lazy val system: ActorSystem = ActorSystem("kafka-message-scheduler")
 
 }

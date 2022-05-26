bdbff84f85cd0b80a7e8c83af7cd86b91c450d51
Scala Steward <disco-scala-steward-github-user@skyglobal.onmicrosoft.com>
2022-05-26 06:34:16 +0000

    Update janino from 3.1.6 to 3.1.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
   val monix            = "io.monix"                   %% "monix-execution"    % "3.4.1"
   val scalaLogging     = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.5"
 
-  val janino         = "org.codehaus.janino"  % "janino"                   % "3.1.6"  % Runtime
+  val janino         = "org.codehaus.janino"  % "janino"                   % "3.1.7"  % Runtime
   val logbackClassic = "ch.qos.logback"       % "logback-classic"          % "1.2.11" % Runtime
   val logbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "7.0.1"  % Runtime
 

6bb44b2fb868337fb7cf1cdc018c5eeaf6179571
Scala Steward <disco-scala-steward-github-user@skyglobal.onmicrosoft.com>
2022-05-26 11:35:03 +0000

    Update kafka-topic-loader from 1.5.6 to 1.5.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -56,7 +56,7 @@ object Dependencies {
   }
 
   val avro4s           = "com.sksamuel.avro4s"        %% "avro4s-core"        % "4.0.13"
-  val kafkaTopicLoader = "uk.sky"                     %% "kafka-topic-loader" % "1.5.6"
+  val kafkaTopicLoader = "uk.sky"                     %% "kafka-topic-loader" % "1.5.7"
   val monix            = "io.monix"                   %% "monix-execution"    % "3.4.1"
   val scalaLogging     = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.5"
 

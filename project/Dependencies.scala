535ca5daaaef3bc12ec3fc7baf882f484d8bd324
Scala Steward <88037287+disco-scala-steward@users.noreply.github.com>
2022-08-04 21:43:23 +0100

    Update embedded-kafka from 3.2.0 to 3.2.1 (#148)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -64,7 +64,7 @@ object Dependencies {
   val logbackClassic = "ch.qos.logback"       % "logback-classic"          % "1.2.11" % Runtime
   val logbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "7.2"    % Runtime
 
-  val embeddedKafka        = "io.github.embeddedkafka" %% "embedded-kafka"              % "3.2.0"    % Test
+  val embeddedKafka        = "io.github.embeddedkafka" %% "embedded-kafka"              % "3.2.1"    % Test
   val mockito              = "org.mockito"              % "mockito-core"                % "4.6.1"    % Test
   val randomDataGenerator  = "com.danielasfregola"     %% "random-data-generator"       % "2.9"      % Test
   val scalaCheck           = "org.scalacheck"          %% "scalacheck"                  % "1.16.0"   % Test

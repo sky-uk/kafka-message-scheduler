484b41e2d8f1027cd9368e0874ce2eb34c42bc35
Scala Steward <88037287+disco-scala-steward@users.noreply.github.com>
2022-09-22 20:20:09 +0100

    Update scalacheck from 1.16.0 to 1.17.0 (#162)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -67,7 +67,7 @@ object Dependencies {
   val embeddedKafka        = "io.github.embeddedkafka" %% "embedded-kafka"              % "3.2.1"    % Test
   val mockito              = "org.mockito"              % "mockito-core"                % "4.8.0"    % Test
   val randomDataGenerator  = "com.danielasfregola"     %% "random-data-generator"       % "2.9"      % Test
-  val scalaCheck           = "org.scalacheck"          %% "scalacheck"                  % "1.16.0"   % Test
+  val scalaCheck           = "org.scalacheck"          %% "scalacheck"                  % "1.17.0"   % Test
   val scalaCheckDatetime   = "com.47deg"               %% "scalacheck-toolbox-datetime" % "0.6.0"    % Test
   val scalaTest            = "org.scalatest"           %% "scalatest"                   % "3.2.13"   % Test
   val scalaTestPlusMockito = "org.scalatestplus"       %% "mockito-3-12"                % "3.2.10.0" % Test

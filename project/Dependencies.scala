d42e4ce128968fce4b881a076662bf02308f9ab3
Scala Steward <88037287+disco-scala-steward@users.noreply.github.com>
2023-03-15 14:21:50 +0000

    Update logback-classic from 1.4.5 to 1.4.6 (#220)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -61,7 +61,7 @@ object Dependencies {
   val scalaLogging     = "com.typesafe.scala-logging" %% "scala-logging"      % "3.9.5"
 
   val janino         = "org.codehaus.janino"  % "janino"                   % "3.1.9" % Runtime
-  val logbackClassic = "ch.qos.logback"       % "logback-classic"          % "1.4.5" % Runtime
+  val logbackClassic = "ch.qos.logback"       % "logback-classic"          % "1.4.6" % Runtime
   val logbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "7.3"   % Runtime
 
   val embeddedKafka        = "io.github.embeddedkafka" %% "embedded-kafka"              % "3.4.0"    % Test

8e672f62531d08cedd633b6174216b962795a8ba
Ben Carter <benjohn.carter@gmail.com>
2023-11-26 00:15:45 +0000

    Fix sbt schema logging

--- avro/src/main/scala/uk/sky/scheduler/avro/Main.scala
+++ avro/src/main/scala/uk/sky/scheduler/avro/Main.scala
@@ -1,5 +1,6 @@
 package uk.sky.scheduler.avro
 
+import cats.effect.Resource.ExitCase
 import cats.effect.{IO, IOApp}
 import cats.syntax.all.*
 import fs2.*
@@ -8,17 +9,20 @@ import uk.sky.scheduler.kafka.avro.avroScheduleCodec
 
 object Main extends IOApp.Simple {
 
+  val schemaPath: Path = Path("target/schemas/schedule.avsc")
+
   override def run: IO[Unit] = {
-    val path = Path("target/schemas/schedule.avsc")
-    {
-      for {
-        schema <- Stream.fromEither[IO](avroScheduleCodec.schema.leftMap(_.throwable))
-        _      <- Stream.eval(Files[IO].createDirectories(Path("target/schemas")))
-        _      <- Stream(schema.toString(true))
-                    .through(text.utf8.encode)
-                    .through(Files[IO].writeAll(path))
-      } yield ()
-    }.compile.drain *> IO.println(s"Generated schema file: $path")
-  }
+    for {
+      schema <- Stream.fromEither[IO](avroScheduleCodec.schema.leftMap(_.throwable))
+      _      <- Stream.eval(Files[IO].createDirectories(Path("target/schemas")))
+      _      <- Stream(schema.toString(true))
+                  .through(text.utf8.encode)
+                  .through(Files[IO].writeAll(schemaPath))
+    } yield ()
+  }.onFinalizeCase {
+    case ExitCase.Succeeded  => IO.println(s"Generated Schema file: $schemaPath")
+    case ExitCase.Errored(e) => IO.println(s"Error creating Schema file: $e")
+    case ExitCase.Canceled   => IO.println("Canceled")
+  }.compile.drain
 
 }

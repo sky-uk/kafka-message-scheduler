4d45bf2d001a8457b1b0f829998dab75ef67e014
Jake Booker <jake.booker@sky.uk>
2025-02-14 14:00:50 +0000

    removed SchedulePublisher changes

--- scheduler-3/src/main/scala/uk/sky/scheduler/SchedulePublisher.scala
+++ scheduler-3/src/main/scala/uk/sky/scheduler/SchedulePublisher.scala
@@ -1,33 +1,8 @@
 package uk.sky.scheduler
 
-import cats.effect.Async
-import cats.syntax.all.*
-import fs2.*
-import fs2.kafka.*
-import uk.sky.scheduler.config.KafkaConfig
-import uk.sky.scheduler.converters.scheduleEvent.*
+import fs2.Pipe
 import uk.sky.scheduler.domain.ScheduleEvent
-import uk.sky.scheduler.kafka.*
 
 trait SchedulePublisher[F[_], O] {
   def publish: Pipe[F, ScheduleEvent, O]
-}
-
-object SchedulePublisher {
-  def kafka[F[_] : Async](config: KafkaConfig): SchedulePublisher[F, Unit] =
-    new SchedulePublisher[F, Unit] {
-      private val producerSettings = config.producerSettings[F, Array[Byte], Option[Array[Byte]]].atLeastOnce.performant
-
-      override def publish: Pipe[F, ScheduleEvent, Unit] = scheduleEventStream =>
-        for {
-          producer <- KafkaProducer.stream(producerSettings)
-          _        <- scheduleEventStream.chunks.evalMap { scheduleEventChunk =>
-                        val producerRecordChunk = scheduleEventChunk.flatMap(scheduleEvent =>
-                          Chunk(scheduleEvent.toProducerRecord, scheduleEvent.toTombstone)
-                        )
-                        producer.produce(producerRecordChunk).flatten
-                      }
-        } yield ()
-    }
-
 }

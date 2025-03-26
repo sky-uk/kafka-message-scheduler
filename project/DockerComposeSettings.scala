55f941a6bacdbdc41d3fa6b8825bcff99a31fa58
tba32 <tatyana.badamshina@sky.uk>
2025-03-26 16:05:56 +0000

    fmt

--- project/DockerComposeSettings.scala
+++ project/DockerComposeSettings.scala
@@ -13,7 +13,8 @@ object DockerComposeSettings {
   }
 
   val kafkaPort        = Map("KAFKA_PORT" -> freePort.toString)
-  val dockerRepository = sys.env.get("DOCKER_REPOSITORY").fold(Map("DOCKER_REPOSITORY" -> "test"))(repo => Map("DOCKER_REPOSITORY" -> repo))
+  val dockerRepository =
+    sys.env.get("DOCKER_REPOSITORY").fold(Map("DOCKER_REPOSITORY" -> "test"))(repo => Map("DOCKER_REPOSITORY" -> repo))
 
   lazy val settings = Seq(
     variablesForSubstitution ++= kafkaPort ++ dockerRepository

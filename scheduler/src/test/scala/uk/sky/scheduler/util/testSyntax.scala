bcc108687a45cf2aa44fbf8c9eb94bcca0b5b883
tba32 <tatyana.badamshina@sky.uk>
2025-04-08 17:17:04 +0100

    Old scheduler removed and new scheduler folder renamed

---
+++ scheduler/src/test/scala/uk/sky/scheduler/util/testSyntax.scala
@@ -0,0 +1,26 @@
+package uk.sky.scheduler.util
+
+import java.time.Instant
+
+import cats.effect.Async
+import cats.effect.syntax.all.*
+import cats.syntax.all.*
+import org.scalatest.exceptions.TestFailedException
+
+import scala.concurrent.duration.*
+
+object testSyntax {
+  extension [F[_] : Async, A](fa: F[A]) {
+    def testTimeout(timeout: FiniteDuration = 10.seconds): F[A] =
+      fa.timeoutTo(
+        timeout,
+        Async[F].raiseError(TestFailedException(s"Operation did not complete within $timeout", 0))
+      )
+
+    def realTimeInstant: F[(Instant, A)] =
+      for {
+        a   <- fa
+        now <- Async[F].realTimeInstant
+      } yield now -> a
+  }
+}

53c9865894bea7f9d35879f4956866b51df089d6
tba32 <tatyana.badamshina@sky.uk>
2025-04-08 17:20:10 +0100

    Dependencies are updated
    Co-authored-by: Daniel Smedley <daniel.smedley@sky.uk>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,18 +2,6 @@ import sbt.*
 
 object Dependencies {
 
-  object Akka {
-    private val version = "2.6.19"
-    val actor           = "com.typesafe.akka" %% "akka-actor"          % version
-    val stream          = "com.typesafe.akka" %% "akka-stream"         % version
-    val streamKafka     = "com.typesafe.akka" %% "akka-stream-kafka"   % "3.0.1"
-    val slf4j           = "com.typesafe.akka" %% "akka-slf4j"          % version
-    val testKit         = "com.typesafe.akka" %% "akka-testkit"        % version % Test
-    val streamTestKit   = "com.typesafe.akka" %% "akka-stream-testkit" % version % Test
-    val base            = Seq(actor, stream, streamKafka, slf4j)
-    val test            = Seq(testKit, streamTestKit)
-  }
-
   object Cats {
     private val version           = "2.7.0"
     private val catsEffectVersion = "3.5.7"
@@ -51,14 +39,6 @@ object Dependencies {
     val test            = Seq(kafka)
   }
 
-  object Kamon {
-    private val version = "2.6.6"
-    val core            = "io.kamon" %% "kamon-core"       % version
-    val akka            = "io.kamon" %% "kamon-akka"       % version
-    val prometheus      = "io.kamon" %% "kamon-prometheus" % version
-    val all             = Seq(core, akka, prometheus)
-  }
-
   object Logstash {
     lazy val logbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "8.0" % Runtime
   }
@@ -69,22 +49,8 @@ object Dependencies {
 
   object PureConfig {
     private val version = "0.17.8"
-    // TODO: Remove if not needed in Scala3
-    val pureconfig      = "com.github.pureconfig" %% "pureconfig"             % version
     val core            = "com.github.pureconfig" %% "pureconfig-core"        % version
-    // TODO: Remove if not needed in Scala3
-    val cats            = "com.github.pureconfig" %% "pureconfig-cats"        % version
     val catsEffect      = "com.github.pureconfig" %% "pureconfig-cats-effect" % version
-    val allScala2       = Seq(pureconfig, cats)
-  }
-
-  object Refined {
-    private val version = "0.9.29"
-    val refined         = "eu.timepit" %% "refined"            % version
-    val pureconfig      = "eu.timepit" %% "refined-pureconfig" % version
-    val scalaCheck      = "eu.timepit" %% "refined-scalacheck" % version % Test
-    val base            = Seq(refined, pureconfig)
-    val test            = Seq(scalaCheck)
   }
 
   object OpenTelemetry {
@@ -119,52 +85,17 @@ object Dependencies {
     lazy val parser  = "io.circe" %% "circe-parser"  % version
   }
 
-  val avro4s           = "com.sksamuel.avro4s"        %% "avro4s-core"            % "4.1.2"
-  val chimney          = "io.scalaland"               %% "chimney"                % "1.5.0"
-  val kafkaTopicLoader = "uk.sky"                     %% "kafka-topic-loader"     % "1.5.6"
-  val fs2TopicLoader   = "uk.sky"                     %% "fs2-kafka-topic-loader" % "0.1.0"
-  val monix            = "io.monix"                   %% "monix-execution"        % "3.4.1"
-  val mouse            = "org.typelevel"              %% "mouse"                  % "1.3.2"
-  val scalaLogging     = "com.typesafe.scala-logging" %% "scala-logging"          % "3.9.5"
+  val avro4s =
+    "com.sksamuel.avro4s" %% "avro4s-core" % "4.1.2" // todo: this will need to be depricated in the issue #126
+  val chimney        = "io.scalaland"  %% "chimney"                % "1.5.0"
+  val fs2TopicLoader = "uk.sky"        %% "fs2-kafka-topic-loader" % "0.1.0"
+  val mouse          = "org.typelevel" %% "mouse"                  % "1.3.2"
 
-  val janino         = "org.codehaus.janino"  % "janino"                   % "3.1.12" % Runtime
-  val logbackClassic = "ch.qos.logback"       % "logback-classic"          % "1.5.15" % Runtime
-  val logbackEncoder = "net.logstash.logback" % "logstash-logback-encoder" % "7.4"    % Runtime
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.5.15" % Runtime
 
-  val embeddedKafka        = "io.github.embeddedkafka" %% "embedded-kafka"              % "3.4.1"    % Test
-  val mockito              = "org.mockito"              % "mockito-core"                % "5.10.0"   % Test
-  val randomDataGenerator  = "com.danielasfregola"     %% "random-data-generator"       % "2.9"      % Test
-  val scalaCheck           = "org.scalacheck"          %% "scalacheck"                  % "1.17.0"   % Test
-  val scalaCheckDatetime   = "com.47deg"               %% "scalacheck-toolbox-datetime" % "0.7.0"    % Test
-  val scalaTest            = "org.scalatest"           %% "scalatest"                   % "3.2.18"   % Test
-  val scalaTestPlusMockito = "org.scalatestplus"       %% "mockito-3-12"                % "3.2.10.0" % Test
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18" % Test
 
-  val core: Seq[ModuleID] =
-    Akka.base ++ Cats.base ++ Kafka.base ++ Kamon.all ++ PureConfig.allScala2 ++ Refined.base ++ Seq(
-      avro4s,
-      kafkaTopicLoader,
-      monix,
-      scalaLogging
-    )
-
-  val runtime: Seq[ModuleID] = Seq(
-    janino,
-    logbackClassic,
-    logbackEncoder
-  )
-
-  val test: Seq[ModuleID]      = Akka.test ++ Cats.test ++ Kafka.test ++ Refined.test ++ Seq(
-    embeddedKafka,
-    mockito,
-    randomDataGenerator,
-    scalaCheck,
-    scalaCheckDatetime,
-    scalaTest,
-    scalaTestPlusMockito
-  )
-  val scheduler: Seq[ModuleID] = core ++ runtime ++ test
-
-  val scheduler3: Seq[ModuleID] = Seq(
+  val core: Seq[ModuleID] = Seq(
     Cats.caseInsensitive,
     Cats.caseInsensitiveTesting,
     Cats.core,

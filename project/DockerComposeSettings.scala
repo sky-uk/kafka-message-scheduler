09fca93d206a84bcd575a455428024006af7d483
Ben Carter <benjohn.carter@gmail.com>
2025-05-17 12:13:53 +0000

    Create topics in docker-compose as part of healthcheck

--- project/DockerComposeSettings.scala
+++ project/DockerComposeSettings.scala
@@ -2,24 +2,10 @@ import DockerPublish.registry
 import com.tapad.docker.DockerComposePlugin.autoImport.variablesForSubstitution
 import sbt.Def
 
-import java.net.ServerSocket
-import scala.util.Using
-
 object DockerComposeSettings {
-
-  def freePort: Int =
-    Using.resource(new ServerSocket(0)) { socket =>
-      socket.setReuseAddress(true)
-      socket.getLocalPort
-    }
-
-  lazy val kafkaPort: (String, String) = "KAFKA_PORT" -> freePort.toString
-
   lazy val settings: Seq[Def.Setting[?]] = Seq(
     variablesForSubstitution ++= Map(
-      "CONTAINER_REPOSITORY" -> registry,
-      kafkaPort
+      "CONTAINER_REPOSITORY" -> registry
     )
   )
-
 }

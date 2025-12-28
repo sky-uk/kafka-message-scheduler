0099f4c8613888cd6a00a0a2f22f06b5c201e903
Ben Carter <benjohn.carter@gmail.com>
2025-12-28 15:15:23 +0000

    feat: improve test resource management

--- scheduler/src/test/scala/uk/sky/scheduler/stubs/StubScheduler.scala
+++ scheduler/src/test/scala/uk/sky/scheduler/stubs/StubScheduler.scala
@@ -2,7 +2,7 @@ package uk.sky.scheduler.stubs
 
 import cats.effect.std.Queue
 import cats.effect.syntax.all.*
-import cats.effect.{Async, Deferred, Fiber, Resource}
+import cats.effect.{Async, Deferred, Resource}
 import cats.syntax.all.*
 import uk.sky.scheduler.*
 import uk.sky.scheduler.domain.ScheduleEvent
@@ -10,22 +10,13 @@ import uk.sky.scheduler.error.ScheduleError
 import uk.sky.scheduler.message.Message
 import uk.sky.scheduler.util.testSyntax.*
 
-final class StubScheduler[F[_] : Async](
+final case class StubScheduler[F[_] : Async](
     events: Queue[F, TestEvent],
     input: Queue[F, Message[Either[ScheduleError, Option[ScheduleEvent]]]],
     eventSubscriber: EventSubscriber[F],
     scheduleQueue: ScheduleQueue[F],
     schedulePublisher: SchedulePublisher[F, ScheduleEvent]
 ) extends Scheduler[F, ScheduleEvent](eventSubscriber, scheduleQueue, schedulePublisher) {
-  def runStreamInBackground: Resource[F, Fiber[F, Throwable, Unit]] =
-    Resource.make {
-      stream
-        .evalTap(event => events.offer(TestEvent.Expired(event)))
-        .compile
-        .drain
-        .start
-    }(_.cancel)
-
   def produce(
       messages: Message[Either[ScheduleError, Option[ScheduleEvent]]]*
   ): F[Unit] =
@@ -39,6 +30,16 @@ final class StubScheduler[F[_] : Async](
 }
 
 object StubScheduler {
+  def resource[F[_] : Async]: Resource[F, StubScheduler[F]] =
+    for {
+      scheduler <- StubScheduler[F]
+      _         <- scheduler.stream
+                     .evalTap(event => scheduler.events.offer(TestEvent.Expired(event)))
+                     .compile
+                     .drain
+                     .background
+    } yield scheduler
+
   def apply[F[_] : Async]: Resource[F, StubScheduler[F]] =
     for {
       events         <- Queue.unbounded[F, TestEvent].toResource

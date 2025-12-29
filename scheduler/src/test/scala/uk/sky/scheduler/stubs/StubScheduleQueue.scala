944ae65051c12b837fb04a53f6e9b299419f8c1a
Ben Carter <benjohn.carter@gmail.com>
2025-12-29 23:20:09 +0000

    feat: add Notifier for scheduler wakeups

--- scheduler/src/test/scala/uk/sky/scheduler/stubs/StubScheduleQueue.scala
+++ scheduler/src/test/scala/uk/sky/scheduler/stubs/StubScheduleQueue.scala
@@ -3,12 +3,12 @@ package uk.sky.scheduler.stubs
 import cats.Parallel
 import cats.effect.std.Queue
 import cats.effect.syntax.all.*
-import cats.effect.{Async, Deferred, Ref, Resource}
+import cats.effect.{Async, Deferred, Resource}
 import cats.syntax.all.*
 import fs2.Stream
 import org.typelevel.otel4s.metrics.Meter
 import uk.sky.scheduler.domain.ScheduleEvent
-import uk.sky.scheduler.{PriorityScheduleQueue, Repository, ScheduleQueue}
+import uk.sky.scheduler.{Notifier, PriorityScheduleQueue, Repository, ScheduleQueue}
 
 final class StubScheduleQueue[F[_] : Async](
     events: Queue[F, TestEvent],
@@ -16,9 +16,9 @@ final class StubScheduleQueue[F[_] : Async](
     repo: Repository[F, String, ScheduleEvent],
     priorityQueue: PriorityScheduleQueue[F],
     outputQueue: Queue[F, ScheduleEvent],
-    wakeupRef: Ref[F, Deferred[F, Unit]]
+    notifier: Notifier[F]
 ) extends ScheduleQueue[F] {
-  private val impl = ScheduleQueue(allowEnqueue, repo, priorityQueue, outputQueue, wakeupRef)
+  private val impl = ScheduleQueue(allowEnqueue, repo, priorityQueue, outputQueue, notifier)
 
   override def schedule(key: String, scheduleEvent: ScheduleEvent): F[Unit] =
     impl.schedule(key, scheduleEvent) *> events.offer(TestEvent.Scheduled(scheduleEvent))
@@ -39,8 +39,7 @@ object StubScheduleQueue {
       repo          <- Repository.ofScalaConcurrentTrieMap[F, String, ScheduleEvent]("test").toResource
       priorityQueue <- PriorityScheduleQueue[F].toResource
       outputQueue   <- Queue.unbounded[F, ScheduleEvent].toResource
-      initialWakeup <- Deferred[F, Unit].toResource
-      wakeupRef     <- Ref.of[F, Deferred[F, Unit]](initialWakeup).toResource
-      _             <- ScheduleQueue.schedulerFiber(allowEnqueue, repo, priorityQueue, outputQueue, wakeupRef).background
-    } yield new StubScheduleQueue(events, allowEnqueue, repo, priorityQueue, outputQueue, wakeupRef)
+      notifier      <- Notifier[F].toResource
+      _             <- ScheduleQueue.schedulerFiber(allowEnqueue, repo, priorityQueue, outputQueue, notifier).background
+    } yield new StubScheduleQueue(events, allowEnqueue, repo, priorityQueue, outputQueue, notifier)
 }
